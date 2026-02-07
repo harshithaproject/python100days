fa3facb72ddc3379f8b2de3e6f114b7d3e94e461
Harshitha Aditham <47668303+harshithaproject@users.noreply.github.com>
2026-02-07 15:51:13 +0530

    Day4- Comments in python

---
+++ day4modules.py
@@ -0,0 +1,24 @@
+print("Hello World") 
+
+str = "Hello World"
+
+print(str[0]) # H
+print(str[1]) # e
+print(str[2]) # l
+print(str[3]) # l
+print(str[4]) # o
+print(str[5]) # (space)
+
+
+#Comments, Escape Sequences, and Print Statements
+
+# This is a single line comment
+""" This is a multi-line comment
+that spans multiple lines
+"""
+
+print("Hello\nWorld") # \n is a newline character
+print("Hello\tWorld") # \t is a tab character
+print("Hello\\World") # \\ is a backslash character
+print("Hello\"World\"") # \" is a double quote character
+print('Hello\'World\'') # \' is a single quote character
