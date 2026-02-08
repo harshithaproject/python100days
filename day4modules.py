5ed7c588ffbf773567aa4d90e65fb5bf8f807327
Harshitha Aditham <47668303+harshithaproject@users.noreply.github.com>
2026-02-08 13:59:22 +0530

    print with tabs

--- day4modules.py
+++ day4modules.py
@@ -17,8 +17,20 @@ print(str[5]) # (space)
 that spans multiple lines
 """
 
+
+'''
+Module - Module is like a code library which can ve used to borrow the code written by somebody else in our program. 
+There are two types of Modules in python
+'''
+
+'''
+Escape Sequences - An escape sequence is a combination of characters that represents a special character when used inside a string.
+
+'''
 print("Hello\nWorld") # \n is a newline character
 print("Hello\tWorld") # \t is a tab character
 print("Hello\\World") # \\ is a backslash character
 print("Hello\"World\"") # \" is a double quote character
 print('Hello\'World\'') # \' is a single quote character
+
+
